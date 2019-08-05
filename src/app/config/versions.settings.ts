83f2c3cfac799117ce34355398821386994322a9
Alexander Görlich <AlexanderGoerlich@users.noreply.github.com>
2019-08-05 15:32:53 +0200

    update release nums

--- src/app/config/versions.settings.ts
+++ src/app/config/versions.settings.ts
@@ -1,17 +1,17 @@
 export const infos = {
     excel: {
         name: 'Sense Excel',
-        version: '3.5.8',
-        download: 'https://m.sense2go.net/asset/57:se-358zip',
+        version: '3.6.7',
+        download: 'https://m.sense2go.net/asset/61:se-367zip',
         reporting: {
             name: 'Sense Excel Reporting',
-            version: '3.5.8',
-            download: 'https://m.sense2go.net/asset/55:ser-358zip'
+            version: '3.6.7',
+            download: 'https://m.sense2go.net/asset/59:ser-367zip'
         },
         fullpackage: {
-            name: 'Sense-Excel-Fullpackage.3.5.8.zip',
-            version: '3.5.8',
-            download: 'https://m.sense2go.net/asset/56:sense-excel-fullpackage358zip'
+            name: 'Sense-Excel-Fullpackage.3.6.7.zip',
+            version: '3.6.7',
+            download: 'https://m.sense2go.net/asset/48:se-fullpackage-current-version'
         },
         smc: {
             name: 'SMC-3.5.8.zip',
